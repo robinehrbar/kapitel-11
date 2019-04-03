fc16d46b8c5316bf142161540d93eabe4e4f9883
Ehrbar Robin <robin.ehrbar@blnet.lokal>
2019-04-03 13:12:24 +0200

    updated the files

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-$('.accordion').on('click','.accordion-titel',function(e){
+/* $('.accordion').on('click','.accordion-titel',function(e){
     e.preventDefault();
     $(this)
-        .next('.accordion-inhalt')
+        .next('.accordionInhalt')
         .not(':animated')
         .slideToggle();
-});
+}); */
+
+var acc = document.getElementsByClassName("accordionTitel");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the accordionInhalt */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active accordionInhalt */
+    var accordionInhalt = this.nextElementSibling;
+    if (accordionInhalt.style.display === "block") {
+      accordionInhalt.style.display = "none";
+    }
+    else {
+      accordionInhalt.style.display = "block";
+    }
+  });
+}
